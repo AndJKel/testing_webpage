00cfe18d1c7920a32dbd8a5de2513995d4a82d23
AndJKel <50556546+AndJKel@users.noreply.github.com>
2024-05-07 21:41:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 5000) {
@@ -16,4 +16,24 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+}
+
+
+
+
+
+
+
+// Search. Information for search obtained from: https://stackoverflow.com/questions/77018595/filter-html-list-based-on-inputs-value
+function filterByName(e) {
+  const items = document.querySelectorAll("ul.article-list li");
+  const searchTerm = e.target.value.trim().toLowerCase();
+
+  items.forEach(item => {
+    item.style.display = 'revert';
+
+    if (!item.innerText.toLowerCase().includes(searchTerm)) {
+      item.style.display = 'none';
+    }
+  })
+}
