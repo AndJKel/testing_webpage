af20492c3eaf912b9b2d254484ec912ad0acd642
AndJKel <50556546+AndJKel@users.noreply.github.com>
2024-05-18 10:53:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// Get the button:
+let mybutton = document.getElementById("UpBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 5000) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+// Search. Information for search obtained from: https://stackoverflow.com/questions/77018595/filter-html-list-based-on-inputs-value
+function filterByName(e) {
+  const items = document.querySelectorAll("ul.article-list li");
+  const searchTerm = e.target.value.trim().toLowerCase();
+
+  items.forEach(item => {
+    item.style.display = 'revert';
+
+    if (!item.innerText.toLowerCase().includes(searchTerm)) {
+      item.style.display = 'none';
+    }
+  })
+}
+
+
+
+
+
+
+
