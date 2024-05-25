8025baaa45ae8349f281b89cdc652c6dfe5a6f32
AndJKel <50556546+AndJKel@users.noreply.github.com>
2024-05-25 11:28:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -36,12 +36,6 @@ function filterByName(e) {
 
 
 
-// Mobile friendly. Information for search obtained from: https://stackoverflow.com/questions/77018595/filter-html-list-based-on-inputs-value
-if (screen.width <= 1000) {
-document.location = "about.html";}
-
-
-
 
 
 
