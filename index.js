4316cd2c290cbd5db28f1468c80d1c24814e8d88
AndJKel <50556546+AndJKel@users.noreply.github.com>
2024-05-25 14:11:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Mobile friendly. Information for search obtained from: https://stackoverflow.com/questions/77018595/filter-html-list-based-on-inputs-value
 if (screen.width <= 1000) {
-document.location = "about.html";}
+document.location = "mobile.html";}
